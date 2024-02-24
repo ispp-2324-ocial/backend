2b34ed16080f7b69e50d1abf196728009de7b920
josecaog26 <j.carlos.ortiz.gutierrez@gmail.com>
2024-02-24 16:19:25 +0100

    feat(subcription): Creación aplicación subcripcion

--- ocial/settings.py
+++ ocial/settings.py
@@ -36,6 +36,7 @@ REST_FRAMEWORK = {
 
 INSTALLED_APPS = [
     'event',
+    'subscription',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    class TypeSubscription(models.TextChoices):
+        FREE = 0, ('Free')
+        BASIC = 1, ('Basic')
+        PRO = 2, ('Pro')
+
+    typeSubscription = models.TextField(
+        choices = TypeSubscription.choices,
+        default = TypeSubscription.FREE
+    )
+    numEvents = models.PositiveIntegerField()
+    canEditEvent = models.BooleanField(default=False)
+    canSendNotifications = models.BooleanField(default=False)
+    canHaveRecurrentEvents = models.BooleanField(default=False)
+    canHaveOustandingEvents = models.BooleanField(default=False)
+    canHaveRating = models.BooleanField(default=False)
+
+
+
+
+    def str(self):
+        return "{}".format(self.typeSubscription)
+
+    def save(self, args, **kwargs):
+        is_new = not self.pk
+        super().save(args, **kwargs)
+
+        if is_new:
+            typeSubscription = self.typeSubscription
