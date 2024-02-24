afff94a8e6ece9365bc19b404f43b383913c1663
josecaog26 <j.carlos.ortiz.gutierrez@gmail.com>
2024-02-24 16:19:25 +0100

    feat(subcription): Creación aplicación subcripcion

--- subscription/tests.py
+++ subscription/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from subscription.models import Subscription
 
-# Create your tests here.
+class SubscriptionTestCase(TestCase):
+    def setUp(self):
+        self.s1 = Subscription.objetcs.create(
+            typeSubscription = Subscription.TypeSubscription.FREE
+        )
+        self.s2 = Subscription.objetcs.create(
+            typeSubscription = Subscription.TypeSubscription.BASIC
+        )
+        self.s2 = Subscription.objetcs.create(
+            typeSubscription = Subscription.TypeSubscription.PRO
+        )
+
+        self.list1 = [self.e1, self.e2, self.e3]
+
+        def test_get_subscriptions(self):
+            self.list2 = [self.e1, self.e2, self.e3]
+            self.assertEquals(self.list1,self.list2)
