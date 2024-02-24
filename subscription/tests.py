66de31df3e6ea34bbac3f4fa10034b0b6441a873
josecaog26 <j.carlos.ortiz.gutierrez@gmail.com>
2024-02-24 16:19:25 +0100

    feat(subcription): Creación aplicación subcripcion

--- subscription/tests.py
+++ subscription/tests.py
@@ -3,17 +3,17 @@ from subscription.models import Subscription
 
 class SubscriptionTestCase(TestCase):
     def setUp(self):
-        self.s1 = Subscription.objects.create(
+        self.s1 = Subscription.objetcs.create(
             typeSubscription = Subscription.TypeSubscription.FREE
         )
-        self.s2 = Subscription.objects.create(
+        self.s2 = Subscription.objetcs.create(
             typeSubscription = Subscription.TypeSubscription.BASIC
         )
-        self.s3 = Subscription.objects.create(
+        self.s2 = Subscription.objetcs.create(
             typeSubscription = Subscription.TypeSubscription.PRO
         )
 
-        self.list1 = [self.s1, self.s2, self.s3]
+        self.list1 = [self.e1, self.e2, self.e3]
 
     def test_get_subscriptions(self):
         self.list2 = [self.s1, self.s2, self.s3]
