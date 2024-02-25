d36d8e73be872b9601e3793f93aa51d1c1741466
iniruimar <iniuimar@us.es>
2024-02-25 12:15:59 +0100

    feat(subscription): Cambios de modelos

--- subscription/tests.py
+++ subscription/tests.py
@@ -3,18 +3,18 @@ from subscription.models import Subscription
 
 class SubscriptionTestCase(TestCase):
     def setUp(self):
-        self.s1 = Subscription.objetcs.create(
+        self.s1 = Subscription.objects.create(
             typeSubscription = Subscription.TypeSubscription.FREE
         )
-        self.s2 = Subscription.objetcs.create(
+        self.s2 = Subscription.objects.create(
             typeSubscription = Subscription.TypeSubscription.BASIC
         )
-        self.s2 = Subscription.objetcs.create(
+        self.s3 = Subscription.objects.create(
             typeSubscription = Subscription.TypeSubscription.PRO
         )
 
-        self.list1 = [self.e1, self.e2, self.e3]
+        self.list1 = [self.s1, self.s2, self.s3]
 
-        def test_get_subscriptions(self):
-            self.list2 = [self.e1, self.e2, self.e3]
-            self.assertEquals(self.list1,self.list2)
+    def test_get_subscriptions(self):
+        self.list2 = [self.s1, self.s2, self.s3]
+        self.assertEquals(self.list1,self.list2)
