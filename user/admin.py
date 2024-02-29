e9308717e69693f3e169c7ba880729d39325119e
carvarsou <carvarsou@alum.us.es>
2024-02-29 19:13:17 +0100

    fix(general): General fixes

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import OcialUser
+from .models import OcialUser, OcialClient
 
 # Register your models here.
 admin.site.register(OcialUser)
+admin.site.register(OcialClient)
