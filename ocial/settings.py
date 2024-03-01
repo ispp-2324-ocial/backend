78d4003a69332fc5fe9769d989213634a5123623
raumonmar1 <raumonmar1@alum.us.es>
2024-03-01 15:33:37 +0100

    chore(user): Updated changes from branch [feat/6-auth]

--- ocial/settings.py
+++ ocial/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
 
 
@@ -48,6 +52,7 @@ INSTALLED_APPS = [
     'user',
     'chat',
     'drf_spectacular',
+    'rest_framework.authtoken',
 ]
 
 STATIC_URL = '/static/'
--- ocial/urls.py
+++ ocial/urls.py
@@ -22,8 +22,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/chat/', include('chat.urls')),
+    path('api/users/', include('user.urls')),
     path('api/event/', include('event.urls')),
-    path('api/', include('user.urls')),
     path('api/doc/openapi.yaml', SpectacularAPIView.as_view(), name='schema'),
     path('api/doc/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/doc/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
