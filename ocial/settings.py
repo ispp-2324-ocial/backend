3ccb56faa91fd8ae4d4326ca083acb09dd49282a
josecaog26 <j.carlos.ortiz.gutierrez@gmail.com>
2024-03-01 19:16:24 +0100

    fix(nearby-event): Corrección de error NearByEvent

--- ocial/settings.py
+++ ocial/settings.py
@@ -41,7 +41,6 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
     'subscription',
     'event',
-    'subscription',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
