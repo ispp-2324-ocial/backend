9ed5296672576832a86030de411f2942bfe603ee
carvarsou <carvarsou@alum.us.es>
2024-03-01 20:52:02 +0100

    feat(event): event list by client

--- event/urls.py
+++ event/urls.py
@@ -4,8 +4,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('list/', views.EventList.as_view(), name='event_list'),
+    path('list/<int:pk>/', views.EventListByClient.as_view(), name='event_list_by_client'),
     path('create/', views.EventCreate.as_view(), name='event_create'),
     # path('<int:event_id>/create/', views.RatingCreate.as_view(), name='rating_create'),
+    path('<int:pk>/update/', views.EventUpdate.as_view(), name='event_update'),
     path('<int:pk>/delete/', views.EventDelete.as_view(), name='event_delete'),
     path('rating/<int:pk>/delete/', views.RatingDelete.as_view(), name='rating_delete'),
     path('nearby/', views.EventNearby.as_view(), name='event_nearby'),
