198814ab087437d307c58ec490c650482bc1260d
raumonmar1 <raumonmar1@alum.us.es>
2024-03-01 14:58:49 +0100

    feat(event): added nearbyEvent

--- event/urls.py
+++ event/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/', views.EventCreate.as_view(), name='event_create'),
     # path('<int:event_id>/create/', views.RatingCreate.as_view(), name='rating_create'),
     path('<int:pk>/delete/', views.EventDelete.as_view(), name='event_delete'),
-    path('rating/<int:pk>/delete/', views.RatingDelete.as_view(), name='rating_delete')
+    path('rating/<int:pk>/delete/', views.RatingDelete.as_view(), name='rating_delete'),
+    path('nearby/', views.EventNearby.as_view(), name='event_nearby'),
 ]
