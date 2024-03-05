f6d112a310776a99dc1ee47c2a9915ba058b83ae
Domi-ATLAS <edupriv2002@gmail.com>
2024-03-05 21:38:59 +0100

    fix(openAPI): resolved openAPI problem

---
+++ ocial/models.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Category(Enum):
+        SPORTS = 0 #, ("Sports")
+        MUSIC = 1 #, ("Music")
+        MARKETS = 2 #, ("Markets")
+        RELAX_ACTIVITIES = 3 #, ("Relax activities")
+        LIVE_CONCERT = 4 #, ("Live concert")
