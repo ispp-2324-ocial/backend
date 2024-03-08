e6d76e6c08e3d15f4192a3b57fe759f198afe421
adrrf <adrromflo@alum.us.es>
2024-03-08 16:33:07 +0100

    fix: category enum

--- ocial/models.py
+++ ocial/models.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Category(Enum):
-    SPORTS = 0  # , ("Sports")
-    MUSIC = 1  # , ("Music")
-    MARKETS = 2  # , ("Markets")
-    RELAX_ACTIVITIES = 3  # , ("Relax activities")
-    LIVE_CONCERT = 4  # , ("Live concert")
+    SPORTS = "Sports"  # , ("Sports")
+    MUSIC = "Music"  # , ("Music")
+    MARKETS = "Markets"  # , ("Markets")
+    RELAX_ACTIVITIES = "Relax activities"  # , ("Relax activities")
+    LIVE_CONCERT = "Live concert"  # , ("Live concert")
