ff4adf7967d97381774c22303e7a77ec27c6ee10
raumonmar1 <raumonmar1@alum.us.es>
2024-03-16 18:09:35 +0100

    test(event): Added event and rating tests

--- event/tests.py
+++ event/tests.py
@@ -1,125 +1,213 @@
-from django.test import TestCase
-from rest_framework.test import APIRequestFactory
-from .models import Event, Rating
-from user.models import OcialClient
 from django.contrib.auth.models import User
-from .views import *
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from .models import Event, OcialClient, Rating
+from .serializers import EventSerializer, RatingSerializer
 
-# Create your tests here.
-
-
-class EventListTestCase(TestCase):
+class EventTestCase(TestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.user_3 = User.objects.create_user(
-            "ocialuser3", "chevy@chase.com", "chevyspassword"
-        )
-        self.client1 = OcialClient.objects.create(
-            usuario=self.user_3,
-            name="A",
-            identification_document="X1234567A",  # nie
-            typeClient=OcialClient.TypeClient.BAR_RESTAURANT,
-            default_latitude=40.7128,
-            default_longitude=0.0,
-        )
-        self.event1 = Event.objects.create(
-            name="Football Match",
-            place="Stadium ABC",
-            event="Match between Team A and Team B",
-            date="2024-02-25",
-            hour="15:00:00",
-            capacity=50000,
-            category=Event.Category.SPORTS,
-            latitude=40.7128,
-            longitude=-74.0060,
-            ocialClient=self.client1,
-        )
-        self.event2 = Event.objects.create(
-            name="Music Festival",
-            place="Park XYZ",
-            event="Annual Music Festival",
-            date="2024-07-15",
-            hour="18:00:00",
-            capacity=10000,
-            category=Event.Category.MUSIC,
-            latitude=34.0522,
-            longitude=-118.2437,
-            ocialClient=self.client1,
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.ocial_client = OcialClient.objects.create(
+            usuario=self.user,
+            default_latitude=0.0,
+            default_longitude=0.0  
         )
 
-    def test_event_list(self):
-        view = EventList.as_view()
-        request = self.factory.get("/event/list/")
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
-        data = response.data
-        self.assertEqual(len(data), 2)  # Verificar que se devuelvan todos los event
+    def test_create_event(self):
+        url = reverse('event_create')
+        data = {
+            'name': 'Test Event',
+            'place': 'Test Place',
+            'event': 'Test Event Description',
+            'date': '2024-03-16',
+            'hour': '12:00:00',
+            'capacity': 100,
+            'category': 'Sports',
+            'latitude': 40.0,
+            'longitude': -40.0,
+            'ocialClient': {
+                'default_latitude': 0.0,  
+                'default_longitude': 0.0  
+            }
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-class RatingListTestCase(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.rating1 = Rating.objects.create()
-        self.rating2 = Rating.objects.create()
-
-    def test_rating_list(self):
-        view = RatingList.as_view()
-        request = self.factory.get("/rating/list/")
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
-        data = response.data
-        self.assertEqual(len(data), 2)  # Verificar que se devuelvan todos los rating
-
-
-class EventCreateTestCase(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-
-    def test_event_create(self):
-        view = EventCreate.as_view()
-        request = self.factory.post("/event/create/", {"id": 1})
-        response = view(request)
-        self.assertEqual(response.status_code, 201)
-        # Verificar si el event se ha creado correctamente
-
-
-class RatingCreateTestCase(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.event = Event.objects.create()
-
-    def test_rating_create(self):
-        view = RatingCreate.as_view()
-        request = self.factory.post(
-            "/rating/1/create/", {"content": "New rating", "event": self.event.id}
+    def test_list_events(self):
+        Event.objects.create(
+            name='Test Event',
+            place='Test Place',
+            event='Test Event Description',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=40.0,
+            longitude=-40.0,
+            ocialClient=self.ocial_client
         )
-        response = view(request)
-        self.assertEqual(response.status_code, 201)
-        # Verificar si el rating se ha creado correctamente
+        url = reverse('event_list')
+        response = self.client.get(url)
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_update_event(self):
+        event = Event.objects.create(
+            name='Test Event',
+            place='Test Place',
+            event='Test Event Description',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=40.0,
+            longitude=-40.0,
+            ocialClient=self.ocial_client
+        )
+        url = reverse('event_update', kwargs={'pk': event.pk})
+        data = {
+            'name': 'Updated Test Event',
+            'place': 'Updated Test Place',
+            'event': 'Updated Test Event Description',
+            'date': '2024-03-16',
+            'hour': '13:00:00',
+            'capacity': 150,
+            'category': 'Music',
+            'latitude': 45.0,
+            'longitude': -45.0,
+            'ocialClient': {  
+                'default_latitude': 0.0,  
+                'default_longitude': 0.0  
+            }
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class EventDeleteTestCase(TestCase):
+    def test_delete_event(self):
+        event = Event.objects.create(
+            name='Test Event',
+            place='Test Place',
+            event='Test Event Description',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=40.0,
+            longitude=-40.0,
+            ocialClient=self.ocial_client
+        )
+        url = reverse('event_delete', kwargs={'pk': event.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_events_by_client(self):
+        url = reverse('event_list_by_client', kwargs={'pk': self.ocial_client.pk})
+        response = self.client.get(url)
+        events = Event.objects.filter(ocialClient=self.ocial_client)
+        serializer = EventSerializer(events, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class EventNearbyTestCase(TestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.event = Event.objects.create()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.ocial_client = OcialClient.objects.create(
+            usuario=self.user,
+            default_latitude=0.0,
+            default_longitude=0.0
+        )
 
-    def test_event_delete(self):
-        view = EventDelete.as_view()
-        request = self.factory.delete(f"/event/{self.event.id}/delete/")
-        response = view(request, pk=self.event.id)
-        self.assertEqual(response.status_code, 204)
-        self.assertFalse(Event.objects.filter(id=self.event.id).exists())
+    def test_list_events_nearby(self):
+        # Crear eventos de prueba
+        event1 = Event.objects.create(
+            name='Test Event 1',
+            place='Test Place 1',
+            event='Test Event Description 1',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=40.0,
+            longitude=-40.0,
+            ocialClient=self.ocial_client
+        )
+        Event.objects.create(
+            name='Test Event 2',
+            place='Test Place 2',
+            event='Test Event Description 2',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=42.0,
+            longitude=-42.0,
+            ocialClient=self.ocial_client
+        )
 
+        url = reverse('event_nearby')
+        data = {'latitude': 40.0, 'longitude': -40.0, 'radius': 10.0}
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class RatingDeleteTestCase(TestCase):
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['id'], event1.id)
+
+class RatingTestCase(TestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.event = Event.objects.create()
-        self.rating = Rating.objects.create(event=self.event, content="Evento")
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.ocial_client = OcialClient.objects.create(
+            usuario=self.user,
+            default_latitude=0.0,
+            default_longitude=0.0
+        )
 
-    def test_message_delete(self):
-        view = RatingDelete.as_view()
-        request = self.factory.delete(f"/event/rating/{self.rating.id}/delete/")
-        response = view(request, pk=self.rating.id)
-        self.assertEqual(response.status_code, 204)
-        self.assertFalse(Rating.objects.filter(id=self.rating.id).exists())
+        self.event = Event.objects.create(
+            name='Test Event',
+            place='Test Place',
+            event='Test Event Description',
+            date='2024-03-16',
+            hour='12:00:00',
+            capacity=100,
+            category='Sports',
+            latitude=40.0,
+            longitude=-40.0,
+            ocialClient=self.ocial_client
+        )
+        
+        self.rating1 = Rating.objects.create(score=4, comment='Great event!', event=self.event)
+        self.rating2 = Rating.objects.create(score=5, comment='Excellent!', event=self.event)
+
+    def test_list_ratings(self):
+        url = reverse('rating_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_rating(self):
+        url = reverse('rating_create')
+        data = {'score': 4, 'comment': 'Great event!', 'event': self.event.pk}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_rating(self):
+        rating = Rating.objects.create(score=4, comment='Great event!', event=self.event)
+        url = reverse('rating_delete', kwargs={'pk': rating.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- event/urls.py
+++ event/urls.py
@@ -3,16 +3,13 @@ from . import views
 
 urlpatterns = [
     path("list/", views.EventList.as_view(), name="event_list"),
-    path(
-        "list/client/<int:pk>/",
-        views.EventListByClient.as_view(),
-        name="event_list_by_client",
-    ),
+    path("list/client/<int:pk>/", views.EventListByClient.as_view(), name="event_list_by_client"),
     path("create/", views.EventCreate.as_view(), name="event_create"),
-    # path('<int:event_id>/create/', views.RatingCreate.as_view(), name='rating_create'),
     path("<int:pk>/update/", views.EventUpdate.as_view(), name="event_update"),
     path("<int:pk>/delete/", views.EventDelete.as_view(), name="event_delete"),
-    path("rating/<int:pk>/delete/", views.RatingDelete.as_view(), name="rating_delete"),
     path("nearby/", views.EventNearby.as_view(), name="event_nearby"),
-    path("<int:pk>/update/", views.EventUpdate.as_view(), name="event_update"),
+    path("rating/list/", views.RatingList.as_view(), name="rating_list"),
+    path("rating/create/", views.RatingCreate.as_view(), name="rating_create"),
+    path("rating/<int:pk>/update/", views.RatingUpdate.as_view(), name="rating_update"),
+    path("rating/<int:pk>/delete/", views.RatingDelete.as_view(), name="rating_delete"),
 ]
