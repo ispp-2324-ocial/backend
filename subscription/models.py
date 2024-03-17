f2cefedb3e5fd90fcdacd4d7aea455f70f70b28d
josecaog26 <j.carlos.ortiz.gutierrez@gmail.com>
2024-03-17 19:00:44 +0100

    feat(subscription): Creación CRUD Modelo Subscriptions

--- subscription/models.py
+++ subscription/models.py
@@ -21,24 +21,10 @@ class Subscription(models.Model):
     )
 
     def __str__(self):
-        return "{}".format(self.typeSubscription)
+        return f"{self.get_typeSubscription_display()} Subscription"
 
     def save(self, *args, **kwargs):
         is_new = not self.pk
-
-        if self.typeSubscription == self.TypeSubscription.FREE:
-            self.numEvents = 1
-        if self.typeSubscription == self.TypeSubscription.BASIC:
-            self.numEvents = 10
-            self.canHaveRating = True
-            self.canEditEvent = True
-        if self.typeSubscription == self.TypeSubscription.PRO:
-            self.numEvents = None
-            self.canHaveRating = True
-            self.canEditEvent = True
-            self.canHaveOustandingEvents = True
-            self.canHaveRecurrentEvents = True
-            self.canSendNotifications = True
         super().save(*args, **kwargs)
 
         if is_new:
@@ -49,25 +35,9 @@ class Subscription(models.Model):
             self.canHaveRecurrentEvents
             self.canHaveOustandingEvents
             self.canHaveRating
+            self.ocialClientId
 
 class SubscriptionForm(ModelForm):
     class Meta:
         model = Subscription
-        fields = "__all__"
-        #fields = []
-    """
-    def clean(self):
-        cleaned_data = super().clean()
-        typeSubscription = cleaned_data.get('subscription_id')
-
-        if typeSubscription:
-            try:
-                subcription_type = Subscription.TypeSubscription(int(typeSubscription))
-            except ValueError:
-                raise forms.ValidationError("Tipo de suscripción no válido")
-            
-            subscription = Subscription(typeSubscription=subcription_type)
-            cleaned_data['subscription'] = subscription
-        
-        return cleaned_data
-    """
+        fields = '__all__'
