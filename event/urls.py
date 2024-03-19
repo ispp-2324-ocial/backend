d6db56b0dfb7e57c19824d6995b9fe70abc866a5
carvarsou <carvarsou@alum.us.es>
2024-03-19 12:49:17 +0100

    fix(event): added missing slashes to urls

--- event/urls.py
+++ event/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     ),
     path("create/", views.EventCreate.as_view(), name="event_create"),
     # path('<int:event_id>/', views.RatingCreate.as_view(), name='rating_create'),
-    path("<int:pk>/update", views.EventUpdate.as_view(), name="event_update"),
-    path("<int:pk>/delete", views.EventDelete.as_view(), name="event_delete"),
+    path("<int:pk>/update/", views.EventUpdate.as_view(), name="event_update"),
+    path("<int:pk>/delete/", views.EventDelete.as_view(), name="event_delete"),
     # path("rating/<int:pk>/", views.RatingDelete.as_view(), name="rating_delete"),
     path("nearby/", views.EventNearby.as_view(), name="event_nearby"),
 ]
