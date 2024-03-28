7324d1dfecc128749bccbefc288ba6823acfe914
ldrakohh <ldrakohh@gmail.com>
2024-03-28 17:27:51 +0100

    fix(test): Arreglados test event, y eliminados tests chat

--- chat/urls.py
+++ chat/urls.py
@@ -2,16 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list/", views.ChatList.as_view(), name="chat_list"),
-    path("<int:pk>/messages", views.ChatDetail.as_view(), name="chat_detail"),
-    path(
-        "<int:chat_id>/createMessage/",
-        views.MessageCreate.as_view(),
-        name="message_create",
-    ),
-    path("<int:pk>/delete/", views.ChatDelete.as_view(), name="chat_delete"),
-    path(
-        "<int:pk>/deleteMessage/", views.MessageDelete.as_view(), name="message_delete"
-    ),
-    path("create/", views.ChatCreate.as_view(), name="chat_create"),
 ]
